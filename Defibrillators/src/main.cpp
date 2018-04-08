1a521bfb86f572cfe4e331eaaf2d48eff9b9242a
Xav83 <x.jouvenot@gmail.com>
2018-04-08 23:19:40 +0200

    Remove using namespace std.

--- Defibrillators/src/main.cpp
+++ Defibrillators/src/main.cpp
@@ -4,8 +4,6 @@
 #include <sstream>
 #include <cmath>
 
-using namespace std;
-
 struct Position
 {
     double longitude;
@@ -14,39 +12,39 @@ struct Position
 
 struct Defibrilator
 {
-    string locationName;
+    std::string locationName;
     Position position;
 };
 
 double distance(const Position& first_position, const Position& second_position)
 {
     //@see distance definition
-    const auto x = (second_position.longitude - first_position.longitude) * cos((first_position.latitude + second_position.latitude) / 2.0);
+    const auto x = (second_position.longitude - first_position.longitude) * std::cos((first_position.latitude + second_position.latitude) / 2.0);
     const auto y = second_position.latitude - first_position.latitude;
-    return sqrt(pow(x, 2.0) + pow(y, 2.0)) * 6371.0;
+    return sqrt(std::pow(x, 2.0) + std::pow(y, 2.0)) * 6371.0;
 }
 
 int main()
 {
-    string LON;
-    cin >> LON; cin.ignore();
-    string LAT;
-    cin >> LAT; cin.ignore();
+    std::string LON;
+    std::cin >> LON; std::cin.ignore();
+    std::string LAT;
+    std::cin >> LAT; std::cin.ignore();
     std::replace(LON.begin(), LON.end(), ',', '.');
     std::replace(LAT.begin(), LAT.end(), ',', '.');
-    Position user {stod(LON), stod(LAT)};
+    Position user {std::stod(LON), stod(LAT)};
 
     int N;
-    cin >> N; cin.ignore();
+    std::cin >> N; std::cin.ignore();
     Defibrilator closest {"NONE", std::numeric_limits<double>::max(), std::numeric_limits<double>::max()};
     for (int i = 0; i < N; i++) {
-        string DEFIB;
-        getline(cin, DEFIB);
+        std::string DEFIB;
+        std::getline(std::cin, DEFIB);
 
-        vector<string> splited_DEFIB;
-        stringstream ss_DEFIB(DEFIB);
-        string token;
-        while(getline(ss_DEFIB, token, ';')) { splited_DEFIB.push_back(token); }
+        std::vector<std::string> splited_DEFIB;
+        std::stringstream ss_DEFIB(DEFIB);
+        std::string token;
+        while(std::getline(ss_DEFIB, token, ';')) { splited_DEFIB.push_back(token); }
         std::replace(splited_DEFIB[splited_DEFIB.size()-2].begin(), splited_DEFIB[splited_DEFIB.size()-2].end(), ',', '.');
         std::replace(splited_DEFIB[splited_DEFIB.size()-1].begin(), splited_DEFIB[splited_DEFIB.size()-1].end(), ',', '.');
         const auto& next_DEFIB = Defibrilator {splited_DEFIB[1], {stod(splited_DEFIB[splited_DEFIB.size()-2]), stod(splited_DEFIB[splited_DEFIB.size()-1])}};
@@ -55,5 +53,5 @@ int main()
             closest = next_DEFIB;
         }
     }
-    cout << closest.locationName << endl;
+    std::cout << closest.locationName << std::endl;
 }
